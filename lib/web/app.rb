0b75cd8597a539915caf6316ce599d39afa9d359
ne006 <IATikhomirov@gmail.com>
2022-03-14 08:59:50 +0300

    Use WebpackHelpers in Web::App

--- lib/web/app.rb
+++ lib/web/app.rb
@@ -5,8 +5,13 @@ require 'sinatra/content_for'
 
 module Web
   class App < Sinatra::Application
+    helpers Sinatra::WebpackHelpers
+
+    manifest File.expand_path('../../public/assets/manifest.json', __dir__)
+
     set :default_content_type, :html
     set :views, File.expand_path('../../views', __dir__)
+    set :public_folder, File.expand_path('../../public/', __dir__)
 
     get '/' do
       slim :index, layout: :application
