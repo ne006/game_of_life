5e052f090a88e87eeb7ddf8151ca37706c4739dc
ne006 <IATikhomirov@gmail.com>
2022-04-08 17:18:42 +0300

    Store

---
+++ lib/store.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require 'singleton'
+
+class Store
+  include Singleton
+
+  attr_accessor :store
+
+  def initialize
+    @store = {}
+  end
+
+  def fetch(key)
+    return store if key.nil? || key == ''
+
+    path = parse_key(key)
+
+    get_entry(path).first
+  end
+
+  def put(key, value)
+    if key.nil? || key == ''
+      old_value = store
+      self.store = value
+
+      return old_value
+    end
+
+    path = parse_key(key)
+
+    old_value, parent = get_entry(path)
+
+    raise ArgumentError, "key \"#{key}\" does not exist" if parent.nil?
+
+    terminal_key = path.last
+    terminal_key = terminal_key.to_i if parent.is_a?(Array)
+
+    parent[terminal_key] = value
+
+    old_value
+  end
+
+  def drop(key)
+    if key.nil? || key == ''
+      old_value = store
+      self.store = nil
+
+      return old_value
+    end
+
+    path = parse_key(key)
+
+    old_value, parent = get_entry(path)
+
+    return nil if parent.nil?
+
+    terminal_key = path.last
+
+    if parent.is_a?(Array)
+      parent.delete_at(terminal_key.to_i)
+    else
+      parent.delete(terminal_key)
+    end
+
+    old_value
+  end
+
+  protected
+
+  def parse_key(key)
+    key.split('.').map! do |v|
+      case v
+      when ~/^\d+$/ then v.to_i
+      else v
+      end
+    end
+  end
+
+  def get_entry(path)
+    path.each_with_index.reduce([nil, store]) do |value_with_parent, key_with_index|
+      value, parent = value_with_parent
+      key, index = key_with_index
+      key = key.to_i if parent.is_a?(Array)
+
+      break [value, parent] if parent.nil?
+
+      value = parent[key]
+
+      break [value, parent] if value.nil? && index.zero?
+      break [value, parent] if index == path.size - 1
+
+      [value, value]
+    end
+  end
+end
