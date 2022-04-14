1b7b8bb5e89f83343ca50280268b600257b74033
ne006 <IATikhomirov@gmail.com>
2022-04-14 10:29:42 +0300

    Store world state on generation, add /world/:uuid/:generation to iterate it further

--- lib/api/app.rb
+++ lib/api/app.rb
@@ -8,9 +8,16 @@ module Api
 
     set :default_content_type, :json
 
-    post '/world' do
-      @world = GameOfLife::World.new(cells: params['cells'], rules: params['rules'])
-      @world.tick(to: params.fetch('generations', @world.generation + 1).to_i)
+    get '/world/:uuid/?:generation?' do
+      @world = Store.instance.fetch("game_of_life.worlds.#{params['uuid']}")
+
+      raise StandardError, "World with UUID #{params['uuid']} not found" unless @world
+
+      generation = @world.generation
+
+      @world.tick(to: (params.fetch('generation') || @world.generation).to_i)
+
+      store(@world, params['uuid']) if generation != @world.generation
 
       json({
              world: {
@@ -19,12 +26,43 @@ module Api
                width: @world.width,
                height: @world.height,
                rules: @world.rulestring
-             }
+             },
+             uuid: params['uuid']
            })
     rescue StandardError => e
+      json({ error: e.message })
+    end
+
+    post '/world' do
+      @world = GameOfLife::World.new(cells: params['cells'], rules: params['rules'])
+      @world.tick(to: params.fetch('generations', @world.generation + 1).to_i)
+
+      @uuid = store(@world)
+
       json({
-             error: e.message
+             world: {
+               geology: @world.geology,
+               generation: @world.generation,
+               width: @world.width,
+               height: @world.height,
+               rules: @world.rulestring
+             },
+             uuid: @uuid
            })
+    rescue StandardError => e
+      json({ error: e.message })
+    end
+
+    protected
+
+    def store(world, uuid = nil)
+      uuid ||= SecureRandom.uuid
+      Store.instance.put('game_of_life', Store.instance.fetch('game_of_life') || {})
+      Store.instance.put('game_of_life.worlds', Store.instance.fetch('game_of_life.worlds') || {})
+      Store.instance.put("game_of_life.worlds.#{uuid}", world)
+      Store.instance.expire("game_of_life.worlds.#{uuid}", Time.now + (60 * 60))
+
+      uuid
     end
   end
 end
